5d3a0d15afebc7959da32c9ad49c0284c16d383c
Glitch (hello-nextjss) <none>
2018-12-09 16:28:07 +0000

    🍜🐑 Checkpoint ./pages/index.js:243863/541

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import { Button } from 'antd'
+      <Button>there's some weird refresh stuff going on here???import { Button } from 'antd'
 import Head from 'next/head'
 
 export default () =>
   <div>
     <Head>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='utf-8' />
-    </Head>
-    <div>
-      <Button>it works, i think!</Button>
+      <meta name='viewport' content='width=device-width, initial-sc      <Button>there's some weird refresh stuff going on here.</Button>
+      <meta charSet='d refresh stuff going on here???</Button>
     </div>
   </div>
 
